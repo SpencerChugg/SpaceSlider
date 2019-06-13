a51991f1648afea3d195734d8e25f160e8fc21db
SpencerChugg <sdchugg@gmail.com>
2019-06-12 23:02:46 -0600

    Menus, Firing Buttons, & New Sprites
    VS [0.011] - 2019-06-01
    Additions:
    - Store, Level, & Main Menu Buttons
    - Level Select Functionality
    - Upgrade Add Functionality
    - Player Upgrade Sprites
    - Shield and Health Bar
    - SpaceSliderLogo
    
    Changes:
    - Replaced Start & Settings Button Sprites
    - NewSprites for Players and Powers

---
+++ Assets/Scripts/OnTouchEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTouchEvent : MonoBehaviour
+{
+    public UnityEvent OnTouch;
+    private void Update()
+    {
+        if (Input.touches.Length > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            OnTouch.Invoke();
+        }       
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerScripts/PlayerShield.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerShield : MonoBehaviour
+{
+    public Image shieldBar;
+    public FloatSO pShield;
+    public FloatSO pMaxShield;
+    public FloatSO pShieldRegen;
+    void Update()
+    {
+        shieldBar.fillAmount = pShield.Value / pMaxShield.Value;
+    }
+}
